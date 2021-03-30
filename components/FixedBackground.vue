7e42ec2b774a0717514ecb5e50bf7f88e2235f4b
Роман Троицкий <vzhyx@ya.ru>
2021-03-31 00:16:41 +0300

    Завёл бэкграунд

---
+++ components/FixedBackground.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-wrapper">
+    <div class="bg-blob" /></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+   name: 'FixedBackground',
+
+  data: () => ({
+      blobsArrayDesktop: [
+        {
+          size: 'xl',
+          color: '#fa3b56',
+          duration: 5,
+        }
+      ]
+  }),
+})
+</script>
+
+<style lang="stylus">
+.bg
+  &-wrapper
+    position fixed
+    width 100vw
+    height 100vh
+    top 0
+    bottom 0
+    left 0
+    right 0
+    z-index -1
+    pointer-events none 
+    user-select none
+
+</style>
