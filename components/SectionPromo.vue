c301ebb6bf10bbc97e4ff6a4b76998086168a452
Роман Троицкий <vzhyx@ya.ru>
2021-04-10 16:50:24 +0300

    Тайтл поправил, баффл, спейсинг, кегль

---
+++ components/SectionPromo.vue
@@ -0,0 +1,145 @@
+<template>
+  <section class="promo">
+    <div class="container flex-desc">
+      <!-- Тайтол -->
+      <div class="promo__block">
+        <h1 class="promo__title">
+          {{ $t('pageTitle') }}
+          <br>
+          <span ref="baffle">{{ $t('pageSubtitle') }}</span>
+        </h1>
+        <div class="promo__description">
+          <p class="promo__description-text">
+            {{ $t('pageDescription') }} <a class="promo__description-link" :href="$t('almaLink')" title="Mente et Malleo" v-text="$t('almaLinkText')" />
+          </p>
+        </div>
+      </div>
+      <!-- Разраб -->
+      <div class="promo__image">
+        <img src="/svg/web-monkey.svg" alt="Web-developer">
+      </div>
+    </div>
+  </section>
+</template>
+
+<i18n>
+{
+  "en": {
+    "pageTitle": "Intro to",
+    "pageSubtitle": "web development",
+    "pageDescription": "Learn to create modern websites and applications at",
+    "almaLinkText": "Russian State University for Geological Prospecting",
+    "almaLink": "https://mgri.ru/en/"
+  },
+  "ru": {
+    "pageTitle": "Введение в",
+    "pageSubtitle": "веб-разработку",
+    "pageDescription": "Научитесь создавать современные сайты и приложения в",
+    "almaLinkText": "РГГРУ им. Серго Орджоникидзе",
+    "almaLink": "https://mgri.ru/"
+  }
+}
+</i18n>
+
+<script lang="ts">
+import random from 'lodash.random'
+// TS ругается на типы, их в баффл не завезли =\
+// @ts-ignore
+import baffle from 'baffle'
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'SectionPromo',
+
+  data: () => ({
+    baffleEl: null,
+    titleTextIndex: 0 as number,
+    titleText: {
+      en: ['web development', 'software development'],
+      ru: ['веб-разработку', 'программирование'],
+    },
+
+  }),
+
+  mounted () {
+    if (!process.client) { return }
+    this.initBaffle()
+    this.initLogoTextAnimation()
+  },
+
+  methods: {
+    // Зобрал с Себура, ибо коротко и то, что нужно
+    initBaffle () {
+      const baffleELement = this.$refs.baffle
+      const parameters = {
+        characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ',
+        speed: 40,
+      }
+      this.baffleEl = baffle(baffleELement).set(parameters)
+    },
+    sleep (ms: number): Promise<boolean> {
+      return new Promise(resolve => setTimeout(resolve, ms))
+    },
+    animateText () {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      this.baffleEl.text((t:any) => this.getTitleTextNext())
+      this.baffleEl.reveal(1000)
+    },
+    getTitleTextNext ():string {
+      const locale = this.$i18n.locale
+      // ТС не может в обращение по индексу такое, по ходу
+      // @ts-ignore
+      const currentTextArray: Array<string> = this.titleText[locale]
+      const currentIndex = (this.titleTextIndex === (currentTextArray.length - 1)) ? 0 : (this.titleTextIndex + 1)
+
+      this.titleTextIndex = currentIndex
+      // @ts-ignore
+      return this.titleText[locale][currentIndex]
+    },
+    initLogoTextAnimation () {
+      const loopAnimateText = async () => {
+        await this.sleep(random(4000, 5000))
+        this.animateText()
+        loopAnimateText()
+      }
+      loopAnimateText()
+    },
+  },
+
+})
+</script>
+
+<style lang="stylus" scoped>
+.promo
+
+  &__block
+    width 100%
+    flex-shrink 0
+    +gt-lg()
+      width 40%
+
+  &__image
+    display flex
+    justify-content center
+    +gt-lg()
+      width 100%
+      justify-content flex-end
+
+  &__title
+    fontBold()
+    +gt-lg()
+      font-size 44px
+      margin-bottom 30px
+
+  &__description
+    fontRegular()
+    +gt-lg()
+      font-size 18px
+
+    &-link
+      fontSemiBold()
+      color #7752BE
+      text-decoration underline
+      text-decoration-color #FF9900
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,103 +1,30 @@
 <!--suppress ES6UnusedImports, TypeScriptCheckImport -->
 <template>
   <div class="page">
-    <section class="promo">
-      <div class="container flex-desc">
-        <!-- Тайтол -->
-        <div class="page__block">
-          <h1 class="page__title">
-            {{ $t('pageTitle') }}
-            <br>
-            <span ref="baffle">{{ $t('pageSubtitle') }}</span>
-          </h1>
-        </div>
-        <!-- Разраб -->
-        <div class="page__image">
-          <img src="/svg/web-monkey.svg" alt="Web-developer">
-        </div>
-      </div>
-    </section>
+    <SectionPromo />
   </div>
 </template>
 
 <i18n>
 {
   "en": {
-    "pageTitle": "Intro to",
-    "pageSubtitle": "web development"
+    "pageTitle": "Intro to web development at MGRI"
   },
   "ru": {
-    "pageTitle": "Введение в",
-    "pageSubtitle": "веб-разработку"
+    "pageTitle": "Введение в веб-разработку в РГГРУ"
   }
 }
 </i18n>
 
 <script lang="ts">
 import Vue from 'vue'
-import random from 'lodash.random'
-// TS ругается на типы, их в баффл не завезли =\
-// @ts-ignore
-import baffle from 'baffle'
+import SectionPromo from '~/components/SectionPromo.vue'
 
 export default Vue.extend({
   layout: 'default',
 
-  data: () => ({
-    baffleEl: null,
-    titleTextIndex: 0 as number,
-    titleText: {
-      en: ['web development', 'software development'],
-      ru: ['веб-разработку', 'программирование'],
-    },
-  }),
-
-  mounted () {
-    if (!process.client) { return }
-
-    this.initBaffle()
-    this.initLogoTextAnimation()
-  },
-
-  methods: {
-    // Зобрал с Себура, ибо коротко и то, что нужно
-    initBaffle () {
-      const baffleELement = this.$refs.baffle
-      const parameters = {
-        characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ',
-        speed: 40,
-      }
-      this.baffleEl = baffle(baffleELement).set(parameters)
-    },
-    sleep (ms: number): Promise<boolean> {
-      return new Promise(resolve => setTimeout(resolve, ms))
-    },
-    animateText () {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      this.baffleEl.text((t:any) => this.getTitleTextNext())
-      this.baffleEl.reveal(1000)
-    },
-    getTitleTextNext ():string {
-      const locale = this.$i18n.locale
-      // ТС не может в обращение по индексу такое, по ходу
-      // @ts-ignore
-      const currentTextArray: Array<string> = this.titleText[locale]
-      const currentIndex = (this.titleTextIndex === (currentTextArray.length - 1)) ? 0 : (currentTextArray.length - 1)
-
-      this.titleTextIndex = currentIndex
-      // @ts-ignore
-      return this.titleText[locale][currentIndex]
-    },
-    initLogoTextAnimation () {
-      const loopAnimateText = async () => {
-        await this.sleep(random(4000, 5000))
-        this.animateText()
-        loopAnimateText()
-      }
-
-      loopAnimateText()
-    },
-
+  components: {
+    SectionPromo,
   },
 
   // мета
@@ -120,9 +47,4 @@ export default Vue.extend({
 <style lang="stylus">
 .page
   padding-top 100px
-
-  &__title
-    fontSemiBold()
-    +gt-lg()
-      font-size 44px
 </style>
